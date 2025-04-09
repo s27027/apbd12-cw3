282ed93c01c2a0c5f7f558858dbfd8379fb5a21a
Lukasz <s27027@pjwstk.edu.pl>
2025-04-09 16:43:12 +0200

    Advanced emp Tests completed

--- Tutorial4/obj/Debug/net9.0/Tutorial4.AssemblyInfo.cs
+++ Tutorial4/obj/Debug/net9.0/Tutorial4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a3f2a1ddba8224705210f24d49174b0ac56769f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3802030da096319858d6091fb3d22f7fe9bcd80c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
